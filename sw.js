82c1c298f57d2f9be7873b980f4e289b456e03d4
Tako <takeshi.a.nakagawa@gmail.com>
2026-04-20 10:02:35 -0500

    Bump SW cache to v6, bypass HTTP cache on fetch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'choyokan-v5';
+const CACHE = 'choyokan-v6';
 const ASSETS = [
   '/Dojo-Maintenance/',
   '/Dojo-Maintenance/index.html',
@@ -23,7 +23,7 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
+    fetch(e.request, { cache: 'no-cache' })
       .then(res => {
         const copy = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, copy));
