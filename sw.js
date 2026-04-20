7dd4a5f398efd168315c56de5e504807db3d5986
Tako <takeshi.a.nakagawa@gmail.com>
2026-04-20 10:00:30 -0500

    Initial commit

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'choyokan-v5';
+const ASSETS = [
+  '/Dojo-Maintenance/',
+  '/Dojo-Maintenance/index.html',
+  '/Dojo-Maintenance/manifest.json',
+  '/Dojo-Maintenance/icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
